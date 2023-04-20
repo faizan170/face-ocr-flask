8aefe1724bb8dedfe7a33417cd8d80d951d340ec
Faizan Amin <faizanfaizanamin@gmail.com>
2023-04-20 15:15:45 +0500

    Schema added

--- views/Face.py
+++ views/Face.py
@@ -1,10 +1,77 @@
 from flask_restful import Resource
+from flask import make_response
 from src.FaceUtils import create_embeddings, compare_embeddings
 from flask import request
 from datetime import datetime
 from config import db
 import os
 users = db.users
+
+user_schema = {
+    "firstName": str,
+    "lastName": str,
+    "email": {
+        "type": str,
+        "required": True,
+        "unique": True,
+    },
+    "mobileNumber": {
+        "type": str,
+        "unique": True,
+    },
+    "mPinCode": str,
+    "emiratesId": str,
+    "countryName": str,
+    "userImage": str,
+    "faceId": str,
+    "nickName": str,
+    "idType": str,
+    "faceImage": str,
+    "avtarId": int,
+    "emoji": str,
+    "password": {
+        "type": str,
+        "required": True,
+    },
+    "verified": {
+        "type": bool,
+        "default": False,
+    },
+    "verifyOtp": str,
+    "resetOtp": str,
+    "stellar": {
+        "publicKey": {
+            "type": str,
+            "default": None,
+        },
+        "secretKey": {
+            "iv": {
+                "type": str,
+                "default": None,
+            },
+            "key": {
+                "type": str,
+                "default": None,
+            },
+            "secretKey": {
+                "type": str,
+                "default": None,
+            },
+        },
+    },
+    "face_embedding": list
+}
+
+
+def process_key(keyData):
+    if type(keyData) == dict and keyData.get("type") == None:
+        final = {}
+        for k, v in keyData.items():
+            final[k] = process_key(v)
+        return final
+    elif type(keyData) == dict and keyData.get("type") != None:
+        return None
+    return None
 
 class FaceEmbeddings(Resource):
     def post(self):
@@ -13,24 +80,45 @@ class FaceEmbeddings(Resource):
         ---
         swagger_from_file: static/swagger/face/embeddings.yml
         """
+        try:
+            file = request.files['file']
+            name = request.form.get("name", "")
 
-        file = request.files['file']
-        name = request.form.get("name")
+            # Split name to first and last name
+            firstName = name.split(" ")[0]
+            lastName = name.replace(f"{name.split(' ')[0]} ", "") if len(name.split(" ")) > 1 else None
 
-        file.save(file.filename)
+            file.save(file.filename)
 
-        ebmeddings = create_embeddings(file.filename)
-        
-        os.remove(file.filename)
+            # Get embeddings
+            try:
+                ebmeddings = create_embeddings(file.filename)
+            except:
+                os.remove(file.filename)
+                return make_response("No embeddings found", 400)
+            
+            os.remove(file.filename)
+            if not ebmeddings:
+                return make_response("No embeddings found", 400)
 
-        users.insert_one({
-            'name' : name,
-            'embeddings' : ebmeddings,
-            'created_at' : datetime.now()
-        })
-        
 
-        return "Embeddings created for this user"
+            available_data = {
+                "firstName" : firstName if len(firstName) > 0 else None,
+                "lastName" : lastName,
+                'face_embedding' : ebmeddings,
+                'created_at' : datetime.now()
+            }
+            for key, val in user_schema.items():
+                if key not in available_data:
+                    available_data[key] = process_key(val)
+
+            
+            users.insert_one(available_data)
+            
+
+            return "Embeddings created for this user"
+        except:
+            return make_response("Failed to add embeddings", 200)
 
 
 class CompareFaceEmbeddings(Resource):
@@ -55,11 +143,11 @@ class CompareFaceEmbeddings(Resource):
             os.remove(file.filename)
             user = "No user found"
             for user_data in users.find():
-                distance = compare_embeddings(user_data['embeddings'], ebmeddings)
+                distance = compare_embeddings(user_data['face_embedding'], ebmeddings)
 
                 if distance < distance_threshold:
-                    user = user_data['name']    
+                    user = f"{user_data['firstName']} {user_data.get('lastName', '')}".replace("None", "")
                     break    
-            return user
+            return user.strip()
         except Exception as ex:
             return str(ex)
--- views/OCR.py
+++ views/OCR.py
@@ -1,13 +1,64 @@
 from flask_restful import Resource
 from src.OCRUtils import engine
 from flask import request
-from datetime import datetime
+from datetime import datetime, date
 from config import db
 import os
 
-ocr_col = db.ocr
+ocr_col = db.customer
 
-
+customer_schema = {
+    "id": { "type": str },
+    "account": { "type": str },
+    "memo": { "type": str },
+    "muxedAccount": { "type": str },
+    #"first_name": { "type": str },
+    #"last_name": { "type": str, "required": True },
+    "name" : {"type" : str}, # changed from first last name
+    "nationality" : {"type" : str},
+    "passport_number" : {"type" : str},
+    "surname" : {"type" : str},
+    "mother_name" : {"type" : str},
+    "issuing_authority" : {"type" : str},
+    "additional_name": { "type": str },
+    "address_country_code": { "type": str },
+    "state_or_province": { "type": str },
+    "city": { "type": str },
+    "postal_code": { "type": str, "required": True },
+    "address": { "type": str },
+    "mobile_number": { "type": str },
+    "email_address": { "type": str },
+    "date_of_birth" : {"type" : str}, #"birth_date": { "type": "Date" },
+    "place_of_birth" : {"type" : str}, #"birth_place": { "type": str },
+    "birth_country_code": { "type": str },
+    "bank_account_number": { "type": str },
+    "bank_account_type": { "type": str },
+    "bank_number": { "type": str },
+    "bank_phone_number": { "type": str },
+    "bank_branch_number": { "type": str },
+    "clabe_number": { "type": str },
+    "tax_id": { "type": str },
+    "tax_id_name": { "type": str },
+    "occupation": { "type": "Number" },
+    "employer_name": { "type": str },
+    "employer_address": { "type": str },
+    "language_code": { "type": str },
+    "id_type": { "type": str },
+    "id_country_code": { "type": str },
+    "date_of_issue" : {"type" : date}, #"id_issue_date": { "type": "Date" },
+    "date_of_expiry" : {"type" : date}, #"id_expiration_date": { "type": "Date" },
+    "id_number": { "type": str },
+    "photo_id_front": { "type": str },
+    "photo_id_back": { "type": str },
+    "notary_approval_of_photo_id": { "type": "Buffer" },
+    "ip_address": { "type": str },
+    "photo_proof_residence": { "type": str },
+    "sex": { "type": str },
+    "proof_of_income": { "type": "Buffer" },
+    "proof_of_liveness": { "type": "Buffer" },
+    "cbu_number": { "type": str },
+    "cbu_alias": { "type": str }
+}
 
 class OCRView(Resource):
     def post(self):
@@ -18,19 +69,25 @@ class OCRView(Resource):
         """
 
         file = request.files['file']
-        name = request.form.get("name")
-
         file.save(file.filename)
 
         ocr = engine()
         ocr(file.filename)
         word = ocr.result
 
-        ocr_col.insert_one({
+        to_insert = {
             'filename' : file.filename,
-            'data' : word,
             'created_at' : datetime.now()
-        })
+        }
+        to_insert.update(word)
+        
+        for key, val in customer_schema.items():
+            if key not in to_insert:
+                to_insert[key] = None
+
+
+
+        ocr_col.insert_one(to_insert)
 
         os.remove(file.filename)
 
